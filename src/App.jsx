97b9af8c9bda5bf9dbdc40dbf458c453896b24e0
Krystal304 <krystalwilliams864@gmail.com>
2024-11-13 18:13:15 -0500

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,83 @@
-import { useEffect, useState } from 'react'
-import SearchBar from './component/SearchBar.jsx'
-import MovieList from './component/MovieList.jsx'
-import axios from 'axios'
-import './App.css'
+import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+import "./App.css";
+import MovieList from "./component/MovieList";
 
-function App() {
-
-
+const App= () => {
   // set state
-  const apiKey = "ca4a1533"
-  const [movie, setMovie] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const apiKey = "5e12f80e";
+  const [movies, setMovies] = useState([
+    {
+      "Title": "Star Wars: Episode IV - A New Hope",
+            "Year": "1977",
+            "imdbID": "tt0076759",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Star Wars: Episode V - The Empire Strikes Back",
+            "Year": "1980",
+            "imdbID": "tt0080684",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTkxNGFlNDktZmJkNC00MDdhLTg0MTEtZjZiYWI3MGE5NWIwXkEyXkFqcGc@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Star Wars: Episode VI - Return of the Jedi",
+            "Year": "1983",
+            "imdbID": "tt0086190",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNWEwOTI0MmUtMGNmNy00ODViLTlkZDQtZTg1YmQ3MDgyNTUzXkEyXkFqcGc@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Star Wars: Episode VII - The Force Awakens",
+            "Year": "2015",
+            "imdbID": "tt2488496",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Star Wars: Episode I - The Phantom Menace",
+            "Year": "1999",
+            "imdbID": "tt0120915",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BODVhNGIxOGItYWNlMi00YTA0LWI3NTctZmQxZGUwZDEyZWI4XkEyXkFqcGc@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Star Wars: Episode III - Revenge of the Sith",
+            "Year": "2005",
+            "imdbID": "tt0121766",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
+    }
+  ]);
 
 
-  // useEffect 
+  const getMovies = async() => {
+    try{
+    const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=5e12f80e'
+
+    const response = axios.get(url);
+   console.log(response.data);
+   if(response.data && response.data.Search)
+   setMovies(response.data.Search || []);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
 
   useEffect(() => {
-    if (searchTerm) {
-      axios.get(`http://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`)
-        .then((response) => {
-          if (response.data.Search) {
-            setMovie(response.data.Search);
-          } else {
-            setMovie([]);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }, [searchTerm]);
+    getMovies();
+  }, []);
 
 
 return (
-  <div className="App">
-    <SearchBar setSearchTerm={setSearchTerm} />
-    <MovieList movie={movie} />
+  <div className='container-fluid movie-app'>
+    <div className='row'>
+      <MovieList movies={movies}/>
+    </div>
   </div>
-);
-
-}
-export default App
+)
+};
+  
+export default App;
--- src/component/MovieList.jsx
+++ src/component/MovieList.jsx
@@ -3,9 +3,17 @@
 
 import React from 'react'
 
-function MovieList({ movies }) {
+function MovieList({movies}) {  
   return (
-    <div>MovieList</div>
+    <div className='d-flex flex-row flex-wrap'>
+
+      {movies.map((movie) => (
+        <div className='m-3' key={movie.imdbID}>
+          <img src={movie.Poster} alt={movie.Title} />
+          <p>{movie.Year}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
