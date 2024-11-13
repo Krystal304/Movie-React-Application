cc2f2bb51ca14d1eaf6d54ea94f612922c80a0b6
Krystal304 <krystalwilliams864@gmail.com>
2024-11-13 18:40:22 -0500

    update url

--- src/App.jsx
+++ src/App.jsx
@@ -7,57 +7,17 @@ import MovieList from "./component/MovieList";
 const App= () => {
   // set state
   const apiKey = "5e12f80e";
-  const [movies, setMovies] = useState([
-    {
-      "Title": "Star Wars: Episode IV - A New Hope",
-            "Year": "1977",
-            "imdbID": "tt0076759",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode V - The Empire Strikes Back",
-            "Year": "1980",
-            "imdbID": "tt0080684",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTkxNGFlNDktZmJkNC00MDdhLTg0MTEtZjZiYWI3MGE5NWIwXkEyXkFqcGc@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode VI - Return of the Jedi",
-            "Year": "1983",
-            "imdbID": "tt0086190",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNWEwOTI0MmUtMGNmNy00ODViLTlkZDQtZTg1YmQ3MDgyNTUzXkEyXkFqcGc@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode VII - The Force Awakens",
-            "Year": "2015",
-            "imdbID": "tt2488496",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode I - The Phantom Menace",
-            "Year": "1999",
-            "imdbID": "tt0120915",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BODVhNGIxOGItYWNlMi00YTA0LWI3NTctZmQxZGUwZDEyZWI4XkEyXkFqcGc@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode III - Revenge of the Sith",
-            "Year": "2005",
-            "imdbID": "tt0121766",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(" ");
+
+
 
 
   const getMovies = async() => {
     try{
-    const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=5e12f80e'
+    const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`
 
-    const response = axios.get(url);
+    const response = await axios.get(url);
    console.log(response.data);
    if(response.data && response.data.Search)
    setMovies(response.data.Search || []);
@@ -74,6 +34,15 @@ const App= () => {
 return (
   <div className='container-fluid movie-app'>
     <div className='row'>
+      <input
+      type="text"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      placeholder="Search for a movie"
+      />
+      <button onClick={getMovies}>Search</button>
+      </div>
+      <div className='row'>
       <MovieList movies={movies}/>
     </div>
   </div>
