6ecc74f7c78843ccaeed6bdf904f2df38882b761
Krystal304 <krystalwilliams864@gmail.com>
2024-11-13 23:30:27 -0500

    button

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,18 +1,20 @@
 
 
-const Search = ({searchTerm, setSearchTerm}) => {
+const Search = ({ searchTerm, setSearchTerm }) => {
     return (
-        <div className="col col-sm-4">
-            <input className="form-control"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Type to search...">
-
-            
-            </input>
-
-        </div>
-    )
-}
-
-export default Search
+      <div className="col col-sm-4">
+        <input
+          className="form-control"
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Type to search..."
+        />
+        <button className="btn btn-primary" onClick={() => {}}>
+          Search
+        </button>
+      </div>
+    );
+  };
+  
+  export default Search;
